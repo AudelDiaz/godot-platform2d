a47215146dcf0e7b0e910ad486655489fb781d0a
Audel Diaz <audel.diaz@outlook.com>
2024-09-13 19:11:45 -0500

    update pause menu

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,14 +1,16 @@
-extends CanvasLayer
+extends Control
+@onready var color_rect: ColorRect = $CanvasLayer/ColorRect
+@onready var v_box_container: VBoxContainer = $CanvasLayer/VBoxContainer
 
 
 func _ready() -> void:
-	$ColorRect.visible = false
-	$VBoxContainer.visible = false
+	color_rect.visible = false
+	v_box_container.visible = false
 	
 func set_paused():
 	get_tree().paused = not get_tree().paused
-	$ColorRect.visible = not $ColorRect.visible
-	$VBoxContainer.visible = not $VBoxContainer.visible
+	color_rect.visible = not color_rect.visible
+	v_box_container.visible = not v_box_container.visible
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
