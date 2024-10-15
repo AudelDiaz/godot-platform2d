bb03c352df2a40b335e37406ac3dfa7ec472cf4d
Audel Diaz <audel.diaz@outlook.com>
2024-10-14 22:13:36 -0500

    Finish coin animation

--- Scripts/collect_coin_animation.gd
+++ Scripts/collect_coin_animation.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var animated_sprite: AnimatedSprite2D = $"../AnimatedSprite2D"
+
 var coin:Area2D = null
 var tween_distance: float = 30
 var tween_duration: float = 0.5
@@ -11,5 +13,8 @@ func _ready() -> void:
 	
 	
 func _on_body_entered(_body: Node2D) -> void:
-	var tween = get_tree().create_tween().bind_node(coin)
+	var tween = get_tree().create_tween().bind_node(coin).set_parallel(true)
 	tween.tween_property(coin, "position", coin.position + Vector2.UP*tween_distance, tween_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_property(animated_sprite,"self_modulate", Color(Color.WHITE, 0), tween_duration)
+	await tween.finished
+	coin.queue_free()
--- Scripts/trophy.gd
+++ Scripts/trophy.gd
@@ -11,8 +11,9 @@ var tween_duration: float = 1
 
 func _on_body_entered(_body: Node2D) -> void:
 	print("Body has touched Trophy")
-	var tween = get_tree().create_tween().bind_node(trophy)
+	var tween = get_tree().create_tween().bind_node(trophy).set_parallel(true)
 	tween.tween_property(trophy, "position", trophy.position + Vector2.UP*tween_distance, tween_duration).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(sprite_2d,"self_modulate", Color(Color.WHITE, 0), tween_duration)
 	Global.level_finished(1)
 	audio_stream_player_2d.play()
 	timer.start()
