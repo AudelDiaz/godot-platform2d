d624e36531893f1b247477be3ac98682afef1ed0
Audel Diaz <audel.diaz@outlook.com>
2024-11-07 20:46:03 -0500

    fix typo

--- Scripts/credits.gd
+++ Scripts/credits.gd
@@ -1,6 +1,8 @@
 extends Control
+@onready var menu_button: Button = $MenuButton
 
-
+func _ready() -> void:
+	menu_button.grab_focus()
 
 func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
